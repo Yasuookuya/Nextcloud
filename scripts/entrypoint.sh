91c89a4acc7ee0a1ba7e8f1e127f64c1e3baecdd
Yasuo Okuya <101363072+Yasuookuya@users.noreply.github.com>
2026-01-06 15:59:40 +0800

    stable

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -151,9 +151,9 @@ $CONFIG = array (
   'overwrite.cli.url' => 'https://RAILWAY_PUBLIC_DOMAIN_PLACEHOLDER',
   'overwriteprotocol' => 'https',
   'overwritehost' => 'RAILWAY_PUBLIC_DOMAIN_PLACEHOLDER',
-  'trusted_proxies' => 
+  'trusted_proxies' =>
   array (
-    0 => '100.0.0.0/8',
+    0 => '0.0.0.0/0',
   ),
   'memcache.local' => '\\OC\\Memcache\\Redis',
   'redis' => 
